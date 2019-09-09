e9be2de4a14517b36283df39f05024b5a9e57ed2
Jason Boerner <jboerner56@gmail.com>
2019-09-09 14:49:08 -0400

    stylefd component for cells

---
+++ src/Styles/StyledCell.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const StyledCell = styled.div`
+    width: auto;
+    background: rgba(${props => props.color})`
+export default StyledCell;
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import style from '../Styles/Cell.style.css';
+// import style from '../Styles/Cell.style.css';
+import StyledCell from '../Styles/StyledCell';
+import { Blocks } from '../Blocks';
 const Cell = ({type}) => (
-    <div className={style.div}>Cell</div>
+    // will grab color our of the blocks array
+    // using L (one of the block names) to hold the place for the gameplay functions later
+    <StyledCell type={'L'} color={Blocks['L'].color}>cell</StyledCell>
 )
 
 export default Cell;
