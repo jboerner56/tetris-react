8deb05f83c130566f1b39f765daa340bbf8aa886
Jason Boerner <jboerner56@gmail.com>
2019-09-09 10:38:36 -0400

    beginning styling

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import style from '../Styles/Cell.style.css';
 const Cell = ({type}) => (
-    <div>Cell</div>
+    <div className={style.div}>Cell</div>
 )
 
 export default Cell;
