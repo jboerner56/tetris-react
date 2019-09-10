dd5764a05294c6825fadcf42fd8ba90c2b8064d5
Jason Boerner <jboerner56@gmail.com>
2019-09-10 19:30:49 -0400

    styledStage, css for ease of testing, need to change

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Cell from './Cell';
-
+import { StyledStage } from '../Styles/StyledStage';
 const Stage = ({stage}) => (
-    <div>
+    // get width from the first 
+    <StyledStage width={stage[0].length} height={stage.length}>
         {/* map thru stage array while also mapping through the row array as well
             map will render a cell component for each. 
             type is the first type in the cell array (cell component) whis is 0/empty cell with no blocks in it
         */}
         {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]}/>))}
-    </div>
+    </StyledStage>
 )
 
 export default Stage;
