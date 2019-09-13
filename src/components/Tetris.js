183551f64d79e297a3ac5f10130feaa977190443
Jason Boerner <jboerner56@gmail.com>
2019-09-13 14:06:35 -0400

    move player function

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { createStage } from '../createStage';
 import StartButton from './StartButton';
 import Stage from './Stage';
 import Display from './Display';
@@ -14,9 +15,34 @@ const Tetris = () => {
 
 
     console.log('rerender');
+
+    const movePlayer = dir => {
+
+    }
+
+    const dropPlayer = () => {
+
+    }
+    const move = ({ key }) => {
+        if(!gameOver) {
+            // 37 is the keycode for the left arrow key
+            if(key === 37){
+                // use movePlayer function and subtract one to move the block one to the left
+                movePlayer(-1)
+                // 39 is keycode for right arrow
+            } else if(key === 39){
+                // add one to move one block to the right
+                movePlayer(1)
+                // 40 is down arrow keycode
+            } else if(key === 40)
+            // 
+            dropPlayer();
+        }
+    }
         return (
             <div>
-            <StyledTetrisWrapper>
+                {/* wrapper around everything will allow for key presses anything on the screen for gameplay */}
+            <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
                 <StyledTetris>
                     <Stage stage={stage} />
                     <aside>
